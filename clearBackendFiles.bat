5facc73c92754450d05baab55ab5151130aa23f6
MichaelEight <werykintosz@wp.eu>
2024-07-22 22:13:44 +0200

    my tools update

---
+++ clearBackendFiles.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM Navigate to the backend directory
+cd backend
+
+REM Clear the content of extracted and uploads directories
+echo Clearing extracted and uploads directories...
+rmdir /S /Q extracted
+mkdir extracted
+rmdir /S /Q uploads
+mkdir uploads
